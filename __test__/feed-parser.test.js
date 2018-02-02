464cf007f40041e53a970c028ca36c8cdcdb4638
Dominik Winter <dominik.winter92@gmail.com>
2018-02-02 20:17:43 +0100

    Add title and original_title properties for movies

--- __test__/feed-parser.test.js
+++ __test__/feed-parser.test.js
@@ -1,21 +1,27 @@
 (() => {
   'use strict';
 
-  const {test} = require('ava');
+  const {readFileSync}  = require('fs');
+  const {test}          = require('ava');
 
-  const {search} = require('../index');
+  const feedParser = require('../lib/feed-parser');
 
   const urlRegex  = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;  
   const keys      = ['original_title', 'title', 'description', 'date', 'permalink', 'image', 'mirrors'];
 
-  test('Search for "The Avengers" in all categories', async t => {
-    const movies = await search('The Avengers');
+  test.beforeEach(async t => {
+    t.context.feed = readFileSync(`${__dirname}/__data__/feed.xml`);
+  });
+
+  test('Parse feeds', async t => {
+    const movies = await feedParser(t.context.feed);
 
     t.true(Array.isArray(movies));
 
     movies.forEach(movie => {
       t.deepEqual(Object.keys(movie), keys);
 
+      t.true(typeof movie.original_title === 'string');
       t.true(typeof movie.title === 'string');
       t.true(typeof movie.description === 'string');
       t.true(movie.date instanceof Date);
--- __test__/get-newest.test.js
+++ __test__/get-newest.test.js
@@ -6,7 +6,7 @@
   const {getNewest} = require('../index');
 
   const urlRegex  = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;  
-  const keys      = ['title', 'description', 'date', 'permalink', 'image', 'mirrors'];
+  const keys      = ['original_title', 'title', 'description', 'date', 'permalink', 'image', 'mirrors'];
 
   test('Get newest', async t => {
     const movies = await getNewest();
@@ -16,6 +16,7 @@
     movies.forEach(movie => {
       t.deepEqual(Object.keys(movie), keys);
 
+      t.true(typeof movie.original_title === 'string');
       t.true(typeof movie.title === 'string');
       t.true(typeof movie.description === 'string');
       t.true(movie.date instanceof Date);
